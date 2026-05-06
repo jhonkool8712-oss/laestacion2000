b9f912dbe2ffa686febc82e4bee93cddb7684d2b
jhonkool8712-oss <jhonkool8712@gmail.com>
2026-05-06 14:52:40 -0500

    cambio10

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Configuración de conexión MySQL para Alwaysdata
 // En Alwaysdata, usa el hostname completo: mysql-usuario.alwaysdata.net
-$host = 'mysql-jhonathan1.alwaysdata.net';
+$host = 'mysql-jhonatan1.alwaysdata.net';
 $username = 'jhonathan1';
 $password = 'clase123';
-$database = 'laestacion2000_db';
+$database = 'restaurant_db';
 
 $mysqli = new mysqli($host, $username, $password, $database);
 if ($mysqli->connect_errno) {
